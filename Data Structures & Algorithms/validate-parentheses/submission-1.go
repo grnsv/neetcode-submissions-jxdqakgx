b808d2d49084dcd7fef41b95058082cfafdf572e
grnsv <70471000+grnsv@users.noreply.github.com>
2026-04-08 22:46:23 +0300

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,24 @@
+func isValid(s string) bool {
+    stack := []rune{}
+	for _, r := range s {
+		if r == ')' {
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else if r == '}' {
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else if r == ']' {
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, r)
+		}
+	}
+	return len(stack) == 0
+}
