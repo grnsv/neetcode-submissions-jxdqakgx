5446e21f702dc797e9ed5f3c5fe7d63ef0c96f63
grnsv <70471000+grnsv@users.noreply.github.com>
2026-04-22 16:18:52 +0300

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.go
@@ -0,0 +1,32 @@
+func checkInclusion(s1 string, s2 string) bool {
+	if len(s1) > len(s2) {
+		return false
+	}
+
+	var letters [26]uint16
+	for i := range len(s1) {
+		letters[s1[i]-'a']++
+	}
+
+	var windowLetters [26]uint16
+	l := 0
+	for r := range len(s2) {
+		i := s2[r]-'a'
+		windowLetters[i]++
+		if windowLetters[i] == letters[i] && r-l+1 == len(s1) {
+			return true
+		}
+
+		if windowLetters[i] > letters[i] {
+			for s2[l] != s2[r] {
+				windowLetters[s2[l]-'a']--
+				l++
+			}
+
+			windowLetters[s2[l]-'a']--
+			l++
+		}
+	}
+
+	return false
+}
