fa4cedd35f9a8b9063995a943b6b1e8731f28ddb
grnsv <70471000+grnsv@users.noreply.github.com>
2026-05-15 17:56:21 +0300

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func goodNodes(root *TreeNode) int {
+	var dfs func(root *TreeNode, maxVal int) int
+	dfs = func(root *TreeNode, maxVal int) int {
+		if root == nil {
+			return 0
+		}
+
+		res := 0
+		if root.Val >= maxVal {
+			res = 1
+		}
+
+		maxVal = max(maxVal, root.Val)
+		return res + dfs(root.Left, maxVal) + dfs(root.Right, maxVal)
+	}
+
+	return dfs(root, root.Val)
+}
